ddbec7201ae31f0a4626231f22d6a7d30a5c9030
TSaiAlekhya <67145656+alekhya1838@users.noreply.github.com>
2020-06-21 11:57:01 +0530

    Add files via upload

---
+++ Assignment3_Positive_Or_Negative_Or_Zero.py
@@ -0,0 +1,9 @@
+num=int(input("enter a number"))
+if num>=0:
+  if num==0:
+    print("number entered is zero")
+  else:
+    print("number entered is positive") 
+else:    
+    print("number entered is negative")   
+
