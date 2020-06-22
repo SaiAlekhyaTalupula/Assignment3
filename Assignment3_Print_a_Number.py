dfb48c4a6c2d8994af40bdf528e1cf776a4b90d7
TSaiAlekhya <67145656+alekhya1838@users.noreply.github.com>
2020-06-22 09:30:44 +0530

    Add files via upload

---
+++ Assignment3_Print_a_Number.py
@@ -0,0 +1,14 @@
+n=int(input('enter the number in between 1-5'))
+if(n<6):
+  if n==1:
+    print('one')
+  elif(n==2):
+    print('two')  
+  elif(n==3):
+    print('three')  
+  elif(n==4):
+    print('four') 
+  elif(n==5):
+    print('five')   
+  else:
+    print(n,'is not in between 1-5')     
